c31790ff70793aa021cc944ca3b419f9a25603bc
kikiodazie <dodazie@gmail.com>
2022-06-01 17:51:23 +0100

    Connect node app to mongoDB container

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 
 const app = express()
 
+
+mongoose.connect("mongodb://divine:test@mongoDB/?authSource=admin")
+.then(() => console.log("Successfully connected to DB"))
+.catch((e) => console.log(e))
 
 app.get("/", (req, res) => {
     res.send("<h2>Hi There!!!!</h2>");
