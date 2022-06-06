e46448c64d32ee856b6067b524e10a7a4325cfcc
kikiodazie <dodazie@gmail.com>
2022-06-06 17:38:46 +0100

    Add authentication features

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const { default: mongoose } = require("mongoose");
 const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
 
 const postRouter = require("./routes/postRoutes");
+const userRouter = require("./routes/userRoutes");
+
 const app = express()
 
 const mongoDBURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/test?authSource=admin`;
@@ -12,12 +14,16 @@ mongoose
 .then(() => console.log("Successfully connected to DB"))
 .catch((e) => console.log(e))
 
+app.use(express.json());
+
+
 app.get("/", (req, res) => {
     res.send("<h2>Hi There!!!!</h2>");
 } )
 
 
 app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/users", userRouter);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`lisening on port ${port}`))
